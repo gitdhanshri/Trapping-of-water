915148d22dfde139cb1cde7943606274fc280417
gitdhanshri <2022bit049@sggs.ac.in>
2024-01-21 11:09:15 +0530

    Additon of new function in given solution

--- trappingof_water.cpp
+++ trappingof_water.cpp
@@ -1,91 +1,85 @@
 // #include<iostream>
 // using namespace std;
-// int max(int a,int b)
+// int collect_water(int arr[],int size)
 // {
-//     if(a>b)
-    
-//         return a;
-    
-//     else
-    
-//         return b;
-    
-// }
-// int min(int a,int b)
-// {
-//     if(a>b)
+//     int res =0;
+//     int lmax[size],rmax[size];
+//     lmax[0]=arr[0];
+//     for(int i=1;i<size;i++)
 //     {
-//         return b;
+//         lmax[i]=max(arr[i],lmax[i-1]);
 //     }
-//     else
+//     rmax[size-1]=arr[size-1];
+//     for(int i=size-2;i>=0;i--)
 //     {
-//         return a;
+//         rmax[i]=max(arr[i],rmax[i+1]);
 //     }
-// }
-
-// int collected_water(int arr[],int n)
-// {
-//     int right[n];
-//     int left[n];
-//     left[0]=arr[0];
-
-//     for(int i=1;i<n;i++)
+//     for(int i=1;i<size-1;i++)
 //     {
-//         left[i]=max(arr[i-1],arr[i]);
-//     }
-
-//     right[n-1]=arr[n-1];
-//     for(int i=n-2;i>=0;i--)
-//     {
-//       right[i]=max(arr[i+1],arr[i]);  
-//     }
-
-//     int res=0;
-//     for(int i=0;i<n;i++)
-//     {
-//         res+=(min(left[i],right[i])-arr[i]);
+//         res=res+(min(lmax[i],rmax[i]))-arr[i];
 //     }
 //     return res;
+
 // }
 // int main(){
 //     int arr[]={3,1,2,4,0,1,3,2};
 //     int size=sizeof(arr)/sizeof(arr[0]);
  
-// int total_wat=collected_water(arr,size);
+// int total_wat=collect_water(arr,size);
 // cout<<"\nTotal amount of collected water->"<<total_wat;
 //   cout<<"\n";
 // return 0;
 // }
-
 #include<iostream>
 using namespace std;
-int collect_water(int arr[],int size)
+int max(int a,int b)
 {
-    int res =0;
-    int lmax[size],rmax[size];
-    lmax[0]=arr[0];
-    for(int i=1;i<size;i++)
+    if(a>b)
     {
-        lmax[i]=max(arr[i],lmax[i-1]);
+        return a;
     }
-    rmax[size-1]=arr[size-1];
-    for(int i=size-2;i>=0;i--)
-    {
-        rmax[i]=max(arr[i],rmax[i+1]);
+    else{
+        return b;
     }
-    for(int i=1;i<size-1;i++)
-    {
-        res=res+(min(lmax[i],rmax[i]))-arr[i];
-    }
-    return res;
-
 }
-int main(){
-    int arr[]={3,1,2,4,0,1,3,2};
+int min(int a, int b)
+{
+    if(a>b)
+    
+     return b;
+    
+    else
+    return a;
+}
+int count_trap_water(int arr[],int n)
+{
+    int res=0;
+    for(int i=1;i<n-1;i++)
+    {
+int lmax=arr[i];
+    for(int j=0;j<i;j++)
+        {
+
+            lmax=max(lmax,arr[j]);
+        }
+    
+    int rmax=arr[i];
+    for(int j=i+1;j<n;j++)
+    {
+        rmax=max(rmax,arr[j]);
+    }
+    res=res+(min(rmax,lmax))-arr[i];
+   }  
+
+return res;
+}
+int main()
+{
+    int arr[]={3,0,1,2,5};
     int size=sizeof(arr)/sizeof(arr[0]);
- 
-int total_wat=collect_water(arr,size);
-cout<<"\nTotal amount of collected water->"<<total_wat;
-  cout<<"\n";
-return 0;
+    cout<<"\n";
+    int collect_water=count_trap_water(arr,size);
+    cout<<"\n\nAmount of water collectecd by given pillaer is-->>"<<collect_water;
+    cout<<"\n";
+    return 0;
 }
